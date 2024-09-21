e3ba7861b05331d67ed25dc1ed87abd980059236
Nikita <nikita.pymitron@gmail.com>
2024-09-21 22:10:46 +0700

    index.html, main.js

--- main.js
+++ main.js
@@ -1,45 +1,74 @@
 const textElement = document.getElementById('text');
 
-function txtFormatRead(text){
-    textElement.innerHTML = text.replaceAll('\n', '<br>');
+async function openBookHandler(file){
+    const bookParser = new BookParser(file, textElement);
+    bookParser.open();
 }
 
-function unknownFormat(){
-    textElement.innerHTML = 'Unknown book format';
-}
-
-function fb2FormatRead(text){
-    const parser = new DOMParser();
-    const xmlDocument = parser.parseFromString(text, 'text/xml');
-
-    let output = '';
-
-    const bookTitle = xmlDocument.querySelector('description > title-info > book-title').textContent;
-    const bookDate = xmlDocument.querySelector('description > document-info > date').textContent;
-
-    output += `${bookTitle} <br> ${bookDate}`;
-
-    const body = xmlDocument.querySelector('body');
-
-    output += body.innerHTML;
-
-    textElement.innerHTML = output;
-}
-
-async function openBook(file){
-    const FILE_NAME = file.name;
-    const FILE_TEXT = await file.text();
-
-    if(FILE_NAME.endsWith('.txt')){
-        txtFormatRead(FILE_TEXT);
-    } else if(FILE_NAME.endsWith('.fb2')){
-        fb2FormatRead(FILE_TEXT);
-    } else {
-        unknownFormat();
-    }
-}
-
-async function readFile(event){
+async function readFileEvent(event){
     const file = event.target.files.item(0);
-    openBook(file);
+    openBookHandler(file);
 }
+
+textElement.addEventListener('click', (e) => {
+    const selection = window.getSelection();
+
+    let range = selection.getRangeAt(0);
+    let node = selection.anchorNode;
+
+    if(range.toString() === ' '){
+       selection.removeAllRanges();
+        return;
+    }
+
+    while(range.startOffset > 0 && range.toString().indexOf(' ') != 0){
+        range.setStart(node, (range.startOffset - 1));
+    }
+
+    while(
+            [...range.toString()].reverse().join('').indexOf(' ') != 0
+    ){
+        try{
+            range.setEnd(node, (range.endOffset + 1));
+        } catch {
+            break;
+        }
+    }
+
+    if(range.toString() === ' '){
+        selection.removeAllRanges();
+        return;
+    }
+
+    let word = range.toString().toLowerCase();
+
+    let formattedWord = '';
+    for(let i = 0 ; i < word.length; i++){
+        let charCode = word.charCodeAt(i)
+        if(
+            (charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0)) ||
+            (charCode >= 'A'.charCodeAt(0) && charCode <= 'Z'.charCodeAt(0))
+        ){
+            formattedWord += word.charAt(i);
+        } else if(i > 1) {
+            break;
+        }
+    }
+
+    console.log(word);
+    console.log(formattedWord);
+
+    // selection.removeAllRanges();
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${formattedWord}`).then(
+        res => {
+            if(res.status == 200){
+                res.json().then(
+                    result => {
+                        console.log(result);
+                    }
+                )
+            }
+        }
+    );
+});
